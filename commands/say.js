296797403b6a7fc53b5df0e58a2ef4bc965c208a
Saadat <78856225+Saadat01@users.noreply.github.com>
2021-07-31 13:28:29 +0000

    Very small changes You won't notice anything

--- commands/say.js
+++ commands/say.js
@@ -3,8 +3,11 @@ module.exports = {
     description: 'It says what you say',
     execute(cliet, message, args) {
         if(message.author.id === "304584342238265345"){
+
+          message.delete();
           const msg = message.content.slice(4).trim();
           message.channel.send(msg)
+
         }
         else{
           message.channel.send("You do not have permission to use this command <:hahaa:865095062484811776>")
